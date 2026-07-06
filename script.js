5a0a1dc54b8166fd70af739c756b46fb0de87a9c
HSAEnterprises <hsadigitalmarketingagency@gmail.com>
2026-07-06 22:59:27 +0500

    Add menu toggle functionality to navigation

--- script.js
+++ script.js
@@ -141,3 +141,11 @@ if (contactForm) {
 
   });
 }
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+if(menuToggle){
+menuToggle.addEventListener("click",()=>{
+nav.classList.toggle("show-menu");
+});
+}
